3a2402bd2069142471d697668ec71c0a4dbf2cfd
Krajcovic <dusan.krajcovic@gmail.com>
2013-05-21 11:07:44 +0200

    Refactoring.

--- src/cz/monetplus/mashregisterplus/PayBaseActivity.java
+++ src/cz/monetplus/mashregisterplus/PayBaseActivity.java
@@ -39,10 +39,7 @@ public class PayBaseActivity extends Activity {
 	private static final int ACTIVITY_INTENT_ID = 33333;
 
 	// Intent request codes
-	private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
-	private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
-	private static final int REQUEST_ENABLE_BT = 3;
-	private static final int REQUEST_PREFERENCE = 4;
+	private static final int REQUEST_CONNECT_DEVICE_INSECURE = 33334;
 
 	private EditText mAmountIdEditText;
 	private Spinner mCurrencySpinner;
